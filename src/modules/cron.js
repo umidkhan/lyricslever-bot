259bdb4d8391120684c5de654f6ef6260736f763
umidkhan <umidxondeveloper@gmail.com>
2024-12-14 18:10:58 +0500

    cron.js changed

--- src/modules/cron.js
+++ src/modules/cron.js
@@ -1,14 +1,16 @@
 const cron = require("node-cron");
 const axios = require("axios");
-require("dotenv").config();
+require("dotenv").config()
 
 const task = cron.schedule("* * * * *", async () => {
-  try {
-    const response = await axios.post(process.env.WEBHOOK_URI);
-    console.log(`POST request sent: ${response.data}`);
-  } catch (error) {
-    console.error(error);
-  }
+	try {
+		const response = await axios.post(
+			process.env.WEBHOOK_URI
+		);
+		console.log(`POST reqest sent: ${response.data}`);
+	} catch (err) {
+		console.error(err);
+	}
 });
 
 module.exports = task;
